1eb0a3358ba2f4c2eb934f1e03aaf327d9ad5640
Yash Rajvanshi <yash526452@gmail.com>
2025-09-01 06:35:29 +0530

    added ScrollToTop and Error Boundary | added env.example

--- src/constants/index.js
+++ src/constants/index.js
@@ -12,7 +12,7 @@ export const EXPERIENCES = [
     year: "June 2024 - July 2024",
     role: "Frontend Developer Intern",
     company: "Ftechiz Solutions",
-    description: `I designed and developed the front-end architecture for two web applications using React.js and CSS preprocessors, creating modular and responsive templates to ensure optimal performance across all devices. Through thorough bug fixes and code reviews, I maintained high code quality and improved application stability. Collaborating with cross-functional teams, I consistently delivered projects ahead of schedule while meeting all requirements. Additionally, I transformed UI/UX prototypes into fully functional, dynamic web applications, ensuring the final product closely aligned with design specifications.`,
+    description: `I built modular and responsive templates using CSS and JavaScript to optimize performance across devices, collaborated with cross-functional teams to meet project deadlines while consistently delivering ahead of schedule, and converted UI/UX prototypes into dynamic web applications that aligned seamlessly with design specifications.`,
     technologies: ["HTML", "CSS", "JavaScript", "React.js"],
   }
 ];
